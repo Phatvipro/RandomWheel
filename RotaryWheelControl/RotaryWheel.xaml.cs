badc8e15214d68405f1aae04b5fd5113e909dc93
VinhhPhu <126315095+VinhhPhu@users.noreply.github.com>
2023-07-12 13:07:15 +0700

    Update RotaryWheel.xaml.cs

--- RotaryWheelControl/RotaryWheel.xaml.cs
+++ RotaryWheelControl/RotaryWheel.xaml.cs
@@ -1,12 +1,14 @@
-﻿// Copyright (c) Microsoft. All rights reserved.
+// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 namespace RotaryWheelUserControl
 {
+    using System;
     using System.Collections.Generic;
     using System.Collections.ObjectModel;
     using System.Collections.Specialized;
     using System.ComponentModel;
+    using System.Diagnostics;
     using System.Linq;
     using System.Runtime.CompilerServices;
     using RotaryWheelUserControl.Extensions;
@@ -29,7 +31,7 @@ namespace RotaryWheelUserControl
         }
 
         private Color _foregroundColor = Colors.White;
-        public Color ForegroundColor 
+        public Color ForegroundColor
         {
             get { return _foregroundColor; }
             set { SetField(ref _foregroundColor, value); }
@@ -114,7 +116,7 @@ namespace RotaryWheelUserControl
 
                 if (SelectedItem != null)
                 {
-                    Angle = 360 - SelectedItem.Angle/2;
+                    Angle = 360 - SelectedItem.Angle / 2;
                 }
             };
 
@@ -147,8 +149,8 @@ namespace RotaryWheelUserControl
         {
             _pieSlices.Clear();
 
-            gridRotateTransform.CenterX = this.RenderSize.Width/2;
-            gridRotateTransform.CenterY = this.RenderSize.Height/2;
+            gridRotateTransform.CenterX = this.RenderSize.Width / 2;
+            gridRotateTransform.CenterY = this.RenderSize.Height / 2;
 
             var startAngle = 0;
             var color = BackgroundColor;
@@ -157,13 +159,13 @@ namespace RotaryWheelUserControl
             {
                 foreach (var slice in Slices)
                 {
-                    var sliceSize = 360/Slices.Count();
+                    var sliceSize = 360 / Slices.Count();
 
                     var pieSlice = new PieSlice
                     {
                         StartAngle = startAngle,
                         Angle = sliceSize,
-                        Radius = Size/2,
+                        Radius = Size / 2,
                         BackgroundColor = color,
                         Label = slice,
                         ForegroundColor = ForegroundColor,
@@ -188,16 +190,63 @@ namespace RotaryWheelUserControl
         private void layoutRoot_ManipulationCompleted(object sender, ManipulationCompletedRoutedEventArgs e)
         {
             var angleFromYAxis = 360 - Angle;
-            SelectedItem = _pieSlices
-                .SingleOrDefault(p => p.StartAngle <= angleFromYAxis && (p.StartAngle + p.Angle) > angleFromYAxis);
-
+            SelectedItem = _pieSlices.SingleOrDefault(p => p.StartAngle <= angleFromYAxis && (p.StartAngle + p.Angle) > angleFromYAxis);
             var finalAngle = SelectedItem.StartAngle + SelectedItem.Angle / 2;
-
             doubleAnimation.From = Angle;
             doubleAnimation.To = 360 - finalAngle;
+            storyBoard.SpeedRatio = 0.1;
             storyBoard.Begin();
+            storyBoard.Completed += (_, args) =>
+            {
+
+            };
+
+
 
             Angle = 360 - finalAngle;
+        }
+
+        /// <summary>
+        /// spin the wheel with defined angle
+        /// </summary>
+        /// <param name="angle"> spin angle
+        /// </param>
+        public void Spin(double angle)
+        {
+            Angle = angle;
+            layoutRoot_ManipulationCompleted(null, null);
+        }
+
+        private void phase1(object _, object args)
+        {
+            storyBoard.Completed -= phase1;
+            storyBoard.Completed += phase2;
+            storyBoard.SpeedRatio = 0.3;
+            storyBoard.Begin();
+            Debug.WriteLine("2nd phase");
+        }
+        private void phase2(object _, object args)
+        {
+            storyBoard.Completed -= phase2;
+            storyBoard.SpeedRatio = 0.2;
+            storyBoard.Begin();
+            Debug.WriteLine("3rd phase");
+        }
+
+        public void Spin(int index)
+        {
+            Random rnd = new Random();
+            SelectedItem = _pieSlices[index];
+            var finalAngle = SelectedItem.StartAngle + SelectedItem.Angle / 2;
+            doubleAnimation.From = Angle;
+            doubleAnimation.To = 360 * 5 - finalAngle;
+            storyBoard.SpeedRatio = 0.5;
+            storyBoard.Begin();
+            Debug.WriteLine("1st phase");
+            storyBoard.Completed += phase1;
+
+            //Angle = 360 - finalAngle;
+
         }
 
         private void SetField<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
@@ -209,5 +258,6 @@ namespace RotaryWheelUserControl
                 eventHandler?.Invoke(this, new PropertyChangedEventArgs(propertyName));
             }
         }
+
     }
 }
